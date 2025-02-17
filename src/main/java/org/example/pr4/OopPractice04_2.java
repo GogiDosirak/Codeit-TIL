4ac06108fb4bda9a19e41729396e6b67640c9da4
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-02-17 14:27:45 +0900

    코드잇 스프린트 TIL (2/17) SOLID - 단일 책임의 원칙 (SRP)

---
+++ src/main/java/org/example/pr4/OopPractice04_2.java
@@ -0,0 +1,16 @@
+package org.example.pr4;
+
+public class OopPractice04_2 {
+    public static void main(String[] args) {
+        UserRepository userRepository = new UserRepository();
+        UserService userService = new UserService(userRepository);
+        userService.registerUser("codeit", "codeit123");
+        String foundPassword = userService.findPassword("codeit");
+        System.out.println(foundPassword);
+
+        AuthService authService = new AuthService(userRepository);
+        boolean authenticated = authService.login("codeit", "codeit123");
+        System.out.println(authenticated);
+
+    }
+}
